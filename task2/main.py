733f5415574c60cba41c430fa3182df103a8731c
Станислав <stasasyadovskiy@gmail.com>
2025-10-25 02:11:55 +0300

    Домашний коммит

--- task2/main.py
+++ task2/main.py
@@ -1,16 +1,18 @@
 # Асядовский Станислав
-import random
-rows = 4
-cols = rows
-nums = [-15, -4, -2, -7, 0, 3, 5, 12, 7]
-matrix = []
-
-for i in range(rows):
-    row = []
-    for j in range(cols):
-        row.append(random.choice(nums))
-         matrix.append(row)
-
-for i in range(rows):
-    for j in range(cols):
-        print(f"{matrix[i][j]:4}", end="")
+import random # подключаем библиотеку для рандомных чисел
+rows = random.randint(4, 8) # рандомнок количество строк
+cols = random.randint(4, 8) # и столбцов
+nums = [-15, -4, -2, -7, 0, 3, 5, 12, 7] # список значений
+matrix = [] # создаём пустой список куда запишем матрицу
+for i in range(rows): # перебираем строки
+    row = [] # создаём пустую строку
+    for j in range(cols): # перебираем столбцы
+        row.append(random.choice(nums)) # добавляем числа в строку
+    matrix.append(row) # добавляем строку в матрицу
+print("Матрица:") 
+for row in matrix: # перебираем строки матрицы
+    for num in row: # перебираем числа в строке
+        print(f"{num:4}", end="") # форматированный вывод
+    print() # переход на новую строку
+odd_sum = sum(num for row in matrix for num in row if num % 2 != 0)
+print("Сумма всех нечётных элементов:", odd_sum) # вывод результата
